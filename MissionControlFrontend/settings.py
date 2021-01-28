548a5512cf3da28a5fac66f247fd24f4574bf0ef
Jonathan David Livesay <jonl316@fastmail.com>
2021-01-28 16:44:29 -0600

    Removed crispy-forms from the installed apps for now until needed later

--- MissionControlFrontend/settings.py
+++ MissionControlFrontend/settings.py
@@ -31,14 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'frontend_app.apps.FrontendAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-crispy-forms',
-    'frontend_app.apps.FrontendAppConfig'
 ]
 
 MIDDLEWARE = [
